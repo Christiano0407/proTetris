3e4a3d4d4f912ebf6b62ff0185701cd4c962a1b8
Christiano0407 <placebo0704@gmail.com>
2021-02-08 08:19:22 -0600

    hamburger

--- src/js/index.js
+++ src/js/index.js
@@ -29,12 +29,42 @@ document.addEventListener(`DOMContentLoader`, () => {
       ]
 
       function createGrid() {
+           // the main grid
           let grid = document.querySelector(".grid")
           for(let i = 0; i < GRID_SIZE; i++) {
               let gridElement = document.createElement("div")
               grid.appendChild(gridElement)
           }
       }
+      //set base of grid
+      for(let i = 0; i < GRID_WIDTH; i++) {
+          let gridElement = document.createElement("div")
+          gridElement.setAttribute("class", "block")
+          grid.appendChild(gridElement)
+      }
 
-      
+      let previouusGrid = document.querySelector(".previous-grid")
+         // Since 16 is the max grid size in which all the Tetrominoes 
+        // can fit in we create one here
+       for(let i = 0; i < 16; i++) {
+           let gridElement = document.createElement("div")
+           previouusGrid.appendChild(gridElement);
+       }  
+       return grid;
+
+       //assign functions to keycodes
+       function control(e){
+           if(e.keyCode === 39)
+           moveright()
+           else if(e.keyCode === 38)
+           rotate()
+           else if(e.keyCode === 37)
+           moveleft()
+           else if (e.keyCode === 40)
+           moveDown()
+       }
+
+       // the classical behavior is to speed up the block if down button is kept pressed so doing that
+          document.addEventListener(`keydown`, control);
+         //The Tetrominoes
 })
